635b31b90728bf0900b8c2da1012342bdbc50a52
Cesar Preza <cpreza93@yahoo.com>
2025-03-06 20:26:30 -0800

    adds sign in component and signout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,44 @@
 import { Link } from 'react-router';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    // Clear the user state
+    setUser(null);
+  };
+
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to='/sign-up'>Sign Up</Link>
-        </li>
-      </ul>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+          <li>
+            <Link to='/'>Dashboard</Link>
+          </li>
+          <li>
+            <Link to='/' onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/sign-in'>Sign In</Link>
+          </li>
+          <li>
+            <Link to='/sign-up'>Sign Up</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
