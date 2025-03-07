4233564dfd8120e254a2f5aafba4e4a8f5c6e34e
Cesar Preza <cpreza93@yahoo.com>
2025-03-06 20:42:31 -0800

    adds token authentication

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,25 @@
-import { useContext } from 'react';
+// src/components/Dashboard/Dashboard.jsx
+
+import { useEffect, useContext } from 'react';
 
 import { UserContext } from '../../contexts/UserContext';
 
+import * as userService from '../../services/userService';
+
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers = await userService.index();
+        console.log(fetchedUsers);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (user) fetchUsers();
+  }, [user]);
 
   return (
     <main>
