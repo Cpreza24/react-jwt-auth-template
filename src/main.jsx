d5efecaf5c5d726f05ea10136d3ff42f87b84fdd
Cesar Preza <cpreza93@yahoo.com>
2025-03-06 19:30:46 -0800

    adds UserProvider and navigates to home after sign up

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // add import for BrowserRouter
+import { UserProvider } from './contexts/UserContext.jsx';
 import './index.css';
 import App from './App.jsx';
 
@@ -10,7 +11,9 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
